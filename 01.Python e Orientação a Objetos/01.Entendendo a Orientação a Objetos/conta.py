b8c6c11696478d1957f657a046c37e4203c57a48
Thiago Rodrigues <57311069+thiagoneye@users.noreply.github.com>
2023-01-17 20:39:09 -0300

    Update conta.py file

--- 01.Python e Orientação a Objetos/01.Entendendo a Orientação a Objetos/conta.py
+++ 01.Python e Orientação a Objetos/01.Entendendo a Orientação a Objetos/conta.py
@@ -8,18 +8,18 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo objeto ... {}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print(f'O saldo do titular {self.titular} é de R$ {self.saldo}.')
+        print(f'O saldo do titular {self.__titular} é de R$ {self.__saldo}.')
 
 # Main
 
 conta1 = Conta(123, "Nico", 55.5, 1000.0)
 conta2 = Conta(321, "Marcos", 100.0, 1000.0)
 
-print(f'O saldo da conta 1 é de R$ {conta1.saldo}.')
+print(f'O saldo da conta 1 é de R$ {conta1._Conta__saldo}.')
 conta1.extrato()
