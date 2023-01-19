30df51dcce53dc8b8d12938907afb4b3a9ddb74d
Thiago Rodrigues <57311069+thiagoneye@users.noreply.github.com>
2023-01-18 22:07:10 -0300

    Create cliente.py

---
+++ 01.Python e Orientação a Objetos/01.Entendendo a Orientação a Objetos/cliente.py
@@ -0,0 +1,28 @@
+"""
+05. Usando Propriedades
+"""
+
+# Classes
+
+class Cliente:
+    def __init__(self, nome):
+        self.__nome = nome
+
+    @property
+    def nome(self):
+        return self.__nome.title()
+
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+
+# Main
+
+cliente = Cliente('nico')
+print(cliente._Cliente__nome)
+print(cliente.nome)
+
+cliente.nome = "marco"
+print(cliente._Cliente__nome)
+print(cliente.nome)
