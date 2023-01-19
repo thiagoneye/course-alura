b6c04721be66bb74c87aaee553330f9b8969e488
Thiago Rodrigues <57311069+thiagoneye@users.noreply.github.com>
2023-01-18 22:07:03 -0300

    Update conta.py

--- 01.Python e Orientação a Objetos/01.Entendendo a Orientação a Objetos/conta.py
+++ 01.Python e Orientação a Objetos/01.Entendendo a Orientação a Objetos/conta.py
@@ -1,6 +1,7 @@
 """
-02. Classes e Objetos.
-03. Usando Métodos.
+02. Classes e Objetos
+03. Usando Métodos
+04. Encapsulamento
 """
 
 # Classes
@@ -19,8 +20,34 @@ class Conta:
     def deposita(self, valor):
         self.__saldo += valor
 
-    def sacar(self, valor):
-        self.__saldo += valor
+    def pode_sacar(self, valor):
+        return valor <= self.__saldo + self.__limite
+
+    def saca(self, valor):
+        if self.pode_sacar(valor):
+            self.__saldo -= valor
+        else:
+            print('O valor é superior ao limite permitido.')
+
+    def transfere(self, destino, valor):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
 
 
 # Main
@@ -30,3 +57,6 @@ conta2 = Conta(321, "Marcos", 100.0, 1000.0)
 
 print(f'O saldo da conta 1 é de R$ {conta1._Conta__saldo}.')
 conta1.extrato()
+
+conta1.saca(1200)
+conta2.saca(200)
