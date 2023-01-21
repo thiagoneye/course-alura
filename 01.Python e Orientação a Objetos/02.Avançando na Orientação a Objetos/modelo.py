0c773161e360e8def370dea5e22e481000f3c2c5
Thiago Rodrigues <57311069+thiagoneye@users.noreply.github.com>
2023-01-21 16:11:48 -0300

    Update modelo.py file

--- 01.Python e Orientação a Objetos/02.Avançando na Orientação a Objetos/modelo.py
+++ 01.Python e Orientação a Objetos/02.Avançando na Orientação a Objetos/modelo.py
@@ -1,75 +1,100 @@
 """
 1. Relembrando Classes e Objetos
 2. Removendo Duplicação com Herança
+3. Reduzindo ifs com Polimorfismo
+4. Quando Não Usar Herança
+5. Duck Typing e um Modelo de Dados
 """
 
 # Classes
 
-class Filme:
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self._ano = ano
+        self._likes = 0
+
+    def dar_likes(self):
+        self._likes += 1
+
+    def __str__(self):
+        return f'Nome: {self._nome} - Ano: {self._ano} - Likes: {self._likes}'
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @property
+    def ano(self):
+        return self._ano
+
+    @property
+    def likes(self):
+        return self._likes
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome
-        self.__ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.__likes = 0
 
-    def dar_like(self):
-        self.__likes += 1
+    def __str__(self):
+        return f'Nome: {self._nome} - Ano: {self._ano} - Duração: {self.duracao} - Likes: {self._likes}'
 
-    @property
-    def nome(self):
-        return self.__nome
-
-    @property
-    def ano(self):
-        return self.__ano
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome
-
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome
-        self.__ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.__likes = 0
 
-    def dar_like(self):
-        self.__likes += 1
+    def __str__(self):
+        return f'Nome: {self._nome} - Ano: {self._ano} - Temporadas: {self.temporadas} - Likes: {self._likes}'
+
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome.title()
+        self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
 
     @property
-    def nome(self):
-        return self.__nome
+    def listagem(self):
+        return self._programas
 
     @property
-    def ano(self):
-        return self.__ano
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome
+    def tamanho(self):
+        return len(self._programas)
 
 
 # Main
 
 if __name__ == '__main__':
-    vingadores = Filme('Vingadores - Guerra Infinita', 2018, 160)
+    vingadores = Filme('vingadores guerra infinita', 2018, 160)
+    vingadores.dar_likes()
+    vingadores.dar_likes()
+    vingadores.dar_likes()
 
-    atlanta = Serie('Atlanta', 2018, 2)
-    print(f'Nome: {atlanta.nome}')
-    print(f'Ano: {atlanta.ano}')
-    print(f'Temporada: {atlanta.temporadas}')
+    atlanta = Serie('atlanta', 2018, 2)
+    atlanta.dar_likes()
+    atlanta.dar_likes()
 
-    atlanta.dar_like()
-    atlanta.dar_like()
-    atlanta.dar_like()
+    tmep = Filme('todo mundo em pânico', 1999, 100)
+    tmep.dar_likes()
 
-    print(f'Likes: {atlanta.likes}')
+    demolidor = Serie('demolidor', 2016, 2)
+    demolidor.dar_likes()
+    demolidor.dar_likes()
+    demolidor.dar_likes()
+    demolidor.dar_likes()
+
+    filmes_e_series = [vingadores, atlanta, tmep, demolidor]
+    playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
+
+    print(f'Tamanho da playlist: {playlist_fim_de_semana.tamanho} programas')
+    print(f'Demolidor está na minha playlist? {demolidor in playlist_fim_de_semana}')
+
+    for programa in playlist_fim_de_semana:
+        print(programa)
