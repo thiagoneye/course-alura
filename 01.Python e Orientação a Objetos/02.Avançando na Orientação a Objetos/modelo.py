3c3f27693499875d5f3f3ee569ff4124bde16ef3
Thiago Rodrigues <57311069+thiagoneye@users.noreply.github.com>
2023-01-21 14:02:44 -0300

    Update modelo.py file

--- 01.Python e Orientação a Objetos/02.Avançando na Orientação a Objetos/modelo.py
+++ 01.Python e Orientação a Objetos/02.Avançando na Orientação a Objetos/modelo.py
@@ -1,29 +1,75 @@
 """
 1. Relembrando Classes e Objetos
+2. Removendo Duplicação com Herança
 """
 
 # Classes
 
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
-        self.ano = ano
+        self.__nome = nome
+        self.__ano = ano
         self.duracao = duracao
+        self.__likes = 0
+
+    def dar_like(self):
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @property
+    def ano(self):
+        return self.__ano
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome
 
 class Serie:
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
-        self.ano = ano
+        self.__nome = nome
+        self.__ano = ano
         self.temporadas = temporadas
+        self.__likes = 0
+
+    def dar_like(self):
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @property
+    def ano(self):
+        return self.__ano
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome
 
 
 # Main
 
-def __init__(self):    
+if __name__ == '__main__':
     vingadores = Filme('Vingadores - Guerra Infinita', 2018, 160)
-    print(vingadores.nome)
 
     atlanta = Serie('Atlanta', 2018, 2)
     print(f'Nome: {atlanta.nome}')
     print(f'Ano: {atlanta.ano}')
     print(f'Temporada: {atlanta.temporadas}')
+
+    atlanta.dar_like()
+    atlanta.dar_like()
+    atlanta.dar_like()
+
+    print(f'Likes: {atlanta.likes}')
